17938c0d6e830f9b857aa58db048dba99f41fa9e
Veeraraghavan Sekar <veeraraghavan.sekar@alterway.fr>
2020-06-17 10:31:58 +0200

    Fixed Tupil issue of Dateregex

--- anonymization/anonymizers/dateAnonymizers.py
+++ anonymization/anonymizers/dateAnonymizers.py
@@ -7,12 +7,12 @@ class DateAnonymizer():
     '''
     Replace the dates with fake ones
 
-        Date Formats: DD/MM/YYYY or DD/MM/YY
+        Date Formats: DD/MMM/YYYY or DD.MMM.YYYY  or DD-MMM-YYYY or DD MMM YYYY
     '''
 
     def __init__(self, anonymization: Anonymization):
         self.anonymization = anonymization
-        self.date_regex= r'(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})|(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))|(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})'
+        self.date_regex= r'\d\d(?:\/|-|\.|\s)(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)(?:\/|-|\.|\s)\d{4}'
 
     def anonymize(self, text: str) -> str:
         return  self.anonymization.regex_anonymizer(text,self.date_regex,'date')
